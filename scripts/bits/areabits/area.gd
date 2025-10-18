1e4ef6df77555e601532aa75a7c3013b842c39fb
appellatebaton0@gmail.com <corneilusmaxfield@gmail.com>
2025-10-18 13:38:44 -0500

    H6.2 - More Area-ing
    - Still working on this area system; it's super fun, though! The layer matching is bitwise, which was cool, and the scripts are cleaning up very nicely (way nicer than last time lmao)

--- scripts/bits/areabits/area.gd
+++ scripts/bits/areabits/area.gd
@@ -1,17 +1,20 @@
 @abstract class_name AreaBit extends Bit
 ## Provides functionality to be run by an AreaMaster, with its own mask that has to be met ALONGSIDE the one from the AreaMaster.
 
+## Run when a body / area enters the Master
 func on_body_entered(_body:Node) -> void:
 	pass
 func on_area_entered(_area:Node) -> void:
 	pass
 
+## Run when a body / area exits the Master
 func on_body_exited(_body:Node) -> void:
 	pass
 func on_area_exited(_area:Node) -> void:
 	pass
 
-func while_overlapping_bodies(_bodies:Node) -> void:
+## Always run; effectively _process with inputs for overlapping bodies / areas.
+func while_overlapping_bodies(_bodies:Array[Node], _delta:float) -> void:
 	pass
-func while_overlapping_areas(_area:Node) -> void:
+func while_overlapping_areas(_area:Array[Node], _delta:float) -> void:
 	pass
--- scripts/bits/areabits/area2d.gd
+++ scripts/bits/areabits/area2d.gd
@@ -1,3 +1,5 @@
-class_name AreaBit2D extends AreaBit
+@abstract class_name AreaBit2D extends AreaBit
 ## Acts like an Area2D, with extendable functionality.
 ## needs to be a child of an AreaMasterBit2D to work.
+
+@export_flags_2d_physics var collision_mask := 0
